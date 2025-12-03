f04008874818b55f42876652d73abe417139740f
ShauryaaSharma <shauryasofficial27@gmail.com>
2025-12-04 04:02:40 +0530

    implemented a hoc for managing window states and added the first window - terminal

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from "../components/Navbar.jsx";
 import Welcome from "../components/welcome.jsx"
 import Dock from "../components/Dock.jsx";
+import { terminal as TerminalWindow } from "./windows";
+import gsap from 'gsap';
+import { Draggable } from 'gsap/all';
+gsap.registerPlugin(Draggable);
 
 const App = () => {
   return (
@@ -9,6 +13,8 @@ const App = () => {
        <Navbar />
        <Welcome />
        <Dock />
+
+       <TerminalWindow />
     </main>
   )
 }
